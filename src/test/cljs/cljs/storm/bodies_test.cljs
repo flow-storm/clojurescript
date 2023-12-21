bc130a193f852b84e3d50e75d9aec2c94b8c1585
Juan Monetta <jpmonettas@gmail.com>
2023-12-21 15:27:11 -0300

    Fix tests after removing form-id from expr and return

--- src/test/cljs/cljs/storm/bodies_test.cljs
+++ src/test/cljs/cljs/storm/bodies_test.cljs
@@ -9,10 +9,10 @@
   (let [r (b/tried)]
     (is (= 4 r) "function return should be right.")
     (is (= [[:fn-call "cljs.storm.tests-code.bodies" "tried" [] 375669735]
-            [:expr-exec 2 "3,1" 375669735]
-            [:expr-exec "#object[...]" "3,2,1" 375669735]
+            [:expr-exec 2 "3,1"]
+            [:expr-exec "#object[...]" "3,2,1"]
             [:bind "e" "#object[...]" ""]
-            [:fn-return 4 "3,3,3" 375669735]]
+            [:fn-return 4 "3,3,3"]]
            (u/capture)) "captured traces should match.")))
 
 (deftest letfn-test
@@ -21,11 +21,11 @@
     (is (= [[:fn-call "cljs.storm.tests-code.bodies" "letfn-fn" [] 1673004201]
             [:fn-call "cljs.storm.tests-code.bodies" "square" [2] 1673004201]
             [:bind "x" 2 ""]
-            [:expr-exec 2 "3,1,0,2,1" 1673004201]
-            [:expr-exec 2 "3,1,0,2,2" 1673004201]
-            [:fn-return 4 "3,1,0,2" 1673004201]
-            [:expr-exec 4 "3,2,2" 1673004201]
-            [:fn-return 5 "3,2" 1673004201]]
+            [:expr-exec 2 "3,1,0,2,1"]
+            [:expr-exec 2 "3,1,0,2,2"]
+            [:fn-return 4 "3,1,0,2"]
+            [:expr-exec 4 "3,2,2"]
+            [:fn-return 5 "3,2"]]
            (u/capture)) "captured traces should match.")))
 
 (deftest loops-test
@@ -37,30 +37,30 @@
 
             [:bind "s" 0 "3"]
             [:bind "n" 2 "3"]
-            [:expr-exec 2 "3,2,1,1" -1606941997]
-            [:expr-exec false "3,2,1" -1606941997]
-            [:expr-exec 0 "3,2,3,1,1" -1606941997]
-            [:expr-exec 2 "3,2,3,1,2" -1606941997]
-            [:expr-exec 2 "3,2,3,1" -1606941997]
-            [:expr-exec 2 "3,2,3,2,1" -1606941997]
-            [:expr-exec 1 "3,2,3,2" -1606941997]
+            [:expr-exec 2 "3,2,1,1"]
+            [:expr-exec false "3,2,1"]
+            [:expr-exec 0 "3,2,3,1,1"]
+            [:expr-exec 2 "3,2,3,1,2"]
+            [:expr-exec 2 "3,2,3,1"]
+            [:expr-exec 2 "3,2,3,2,1"]
+            [:expr-exec 1 "3,2,3,2"]
 
             [:bind "s" 2 "3"]
             [:bind "n" 1 "3"]
-            [:expr-exec 1 "3,2,1,1" -1606941997]
-            [:expr-exec false "3,2,1" -1606941997]
-            [:expr-exec 2 "3,2,3,1,1" -1606941997]
-            [:expr-exec 1 "3,2,3,1,2" -1606941997]
-            [:expr-exec 3 "3,2,3,1" -1606941997]
-            [:expr-exec 1 "3,2,3,2,1" -1606941997]
-            [:expr-exec 0 "3,2,3,2" -1606941997]
+            [:expr-exec 1 "3,2,1,1"]
+            [:expr-exec false "3,2,1"]
+            [:expr-exec 2 "3,2,3,1,1"]
+            [:expr-exec 1 "3,2,3,1,2"]
+            [:expr-exec 3 "3,2,3,1"]
+            [:expr-exec 1 "3,2,3,2,1"]
+            [:expr-exec 0 "3,2,3,2"]
 
             [:bind "s" 3 "3"]
             [:bind "n" 0 "3"]
-            [:expr-exec 0 "3,2,1,1" -1606941997]
-            [:expr-exec true "3,2,1" -1606941997]
+            [:expr-exec 0 "3,2,1,1"]
+            [:expr-exec true "3,2,1"]
 
-            [:fn-return 3 "3,2,2" -1606941997]]
+            [:fn-return 3 "3,2,2"]]
            (u/capture)) "captured traces should match.")))
 
 (deftest let-test
@@ -68,15 +68,15 @@
     (is (= 15 r) "function return should be right.")
     (is (= [[:fn-call "cljs.storm.tests-code.bodies" "letter" [] 844078910]
             [:bind "a" 5 "3"]
-            [:expr-exec 5 "3,1,3,1" 844078910]
-            [:expr-exec 10 "3,1,3" 844078910]
+            [:expr-exec 5 "3,1,3,1"]
+            [:expr-exec 10 "3,1,3"]
             [:bind "b" 10 "3"]
-            [:expr-exec 5 "3,1,5,1,1,1" 844078910]
-            [:expr-exec 10 "3,1,5,1,1,2" 844078910]
-            [:expr-exec 15 "3,1,5,1,1" 844078910]
+            [:expr-exec 5 "3,1,5,1,1,1"]
+            [:expr-exec 10 "3,1,5,1,1,2"]
+            [:expr-exec 15 "3,1,5,1,1"]
             [:bind "z" 15 "3,1,5"]
             [:bind "c" 15 "3"]
-            [:fn-return 15 "3,2" 844078910]]
+            [:fn-return 15 "3,2"]]
            (u/capture)) "captured traces should match.")))
 
 (deftest case-test
@@ -84,29 +84,29 @@
     (is (= 42 r) "function return should be right.")
     (is (= [[:fn-call "cljs.storm.tests-code.bodies" "casey" [":first"] -742825312]
             [:bind "x" ":first" ""]
-            [:expr-exec ":first" "3,1" -742825312]
-            [:fn-return 42 "3,3" -742825312]]
+            [:expr-exec ":first" "3,1"]
+            [:fn-return 42 "3,3"]]
            (u/capture)) "captured traces should match.")))
 
 (deftest do-test
   (let [r (b/doer)]
     (is (= 8 r) "function return should be right.")
     (is (= [[:fn-call "cljs.storm.tests-code.bodies" "doer" [] -1895706795]
-            [:expr-exec 2 "3,1" -1895706795]
-            [:expr-exec 4 "3,2" -1895706795]
-            [:expr-exec 6 "3,3,1" -1895706795]
-            [:fn-return 8 "3,3,2" -1895706795]]
+            [:expr-exec 2 "3,1"]
+            [:expr-exec 4 "3,2"]
+            [:expr-exec 6 "3,3,1"]
+            [:fn-return 8 "3,3,2"]]
            (u/capture)) "captured traces should match.")))
 
 (deftest set!-test
   (let [r (b/setter)]
     (is (= 42 r) "function return should be right.")
     (is (= [[:fn-call "cljs.storm.tests-code.bodies" "setter" [] 807580509]
-            [:expr-exec 2 "3" 807580509]
-            [:expr-exec 40 "4" 807580509]
-            #_[:expr-exec 2 "5,1" 807580509] ;; TODO: bring this back
-            #_[:expr-exec 40 "5,2" 807580509]
-            [:fn-return 42 "5" 807580509]]
+            [:expr-exec 2 "3"]
+            [:expr-exec 40 "4"]
+            #_[:expr-exec 2 "5,1"] ;; TODO: bring this back
+            #_[:expr-exec 40 "5,2"]
+            [:fn-return 42 "5"]]
            (u/capture)) "captured traces should match.")))
 
 (deftest interop-test
@@ -114,11 +114,11 @@
     (is (= 42 r) "function return should be right.")
     (is (= [[:fn-call "cljs.storm.tests-code.bodies" "interopter" ["#js {:num 2, :f #object[...]}"] 919708180]
             [:bind "o" "#js {:num 2, :f #object[...]}" ""]
-            [:expr-exec "#js {:num 2, :f #object[...]}" "3,1,1" 919708180]
-            [:expr-exec 2 "3,1" 919708180]
-            [:expr-exec "#js {:num 2, :f #object[...]}" "3,2,1" 919708180]
-            [:expr-exec 40 "3,2" 919708180]
-            [:fn-return 42 "3" 919708180]]
+            [:expr-exec "#js {:num 2, :f #object[...]}" "3,1,1"]
+            [:expr-exec 2 "3,1"]
+            [:expr-exec "#js {:num 2, :f #object[...]}" "3,2,1"]
+            [:expr-exec 40 "3,2"]
+            [:fn-return 42 "3"]]
            (u/capture)) "captured traces should match.")))
 
 (deftest integration-1-test
@@ -126,21 +126,21 @@
     (is (= 4 r) "function return should be right.")
     (is (= [[:fn-call "cljs.storm.tests-code.bodies" "integration-1" [{:x 2}] -608980379]
             [:bind "m" {:x 2} ""]
-            [:expr-exec {:x 2} "3,1,1,1,1" -608980379]
-            [:expr-exec true "3,1,1,1" -608980379]
-            [:expr-exec {:x 2} "3,1,1,2,1" -608980379]
-            [:expr-exec 2 "3,1,1,2" -608980379]
+            [:expr-exec {:x 2} "3,1,1,1,1"]
+            [:expr-exec true "3,1,1,1"]
+            [:expr-exec {:x 2} "3,1,1,2,1"]
+            [:expr-exec 2 "3,1,1,2"]
             [:bind "x" 2 "3"]
-            [:expr-exec 2 "3,2,1" -608980379]
-            [:expr-exec 2 "3,2,2" -608980379]
-            [:fn-return 4 "3,2" -608980379]]
+            [:expr-exec 2 "3,2,1"]
+            [:expr-exec 2 "3,2,2"]
+            [:fn-return 4 "3,2"]]
            (u/capture)) "captured traces should match.")))
 
 (deftest js-fn-call-test
   (let [r (b/js-fn-call)]
     (is (instance? js/Promise r) "function return should be right.")
     (is (= [[:fn-call "cljs.storm.tests-code.bodies" "js-fn-call" [] -166767748]
-            [:fn-return "#object[...]" "3" -166767748]]
+            [:fn-return "#object[...]" "3"]]
            (u/capture)) "captured traces should match.")))
 
 (deftest clj-fn-call-test
@@ -148,9 +148,9 @@
     (is (= {:a {:b 42}, [:c] 42} r) "function return should be right.")
     (is (=  [[:fn-call "cljs.storm.tests-code.bodies" "clj-fn-call" [{}] -1988855813]
              [:bind "m" {} ""]
-             [:expr-exec {} "3,1,1" -1988855813]
-             [:expr-exec {:a {:b 42}} "3,1" -1988855813]
-             [:expr-exec {} "3,2,1" -1988855813]
-             [:expr-exec {[:c] 42} "3,2" -1988855813]
-             [:fn-return {:a {:b 42}, [:c] 42} "3" -1988855813]]
+             [:expr-exec {} "3,1,1"]
+             [:expr-exec {:a {:b 42}} "3,1"]
+             [:expr-exec {} "3,2,1"]
+             [:expr-exec {[:c] 42} "3,2"]
+             [:fn-return {:a {:b 42}, [:c] 42} "3"]]
            (u/capture)) "captured traces should match.")))
--- src/test/cljs/cljs/storm/types_test.cljs
+++ src/test/cljs/cljs/storm/types_test.cljs
@@ -10,18 +10,18 @@
         r (ts/area s)]
     (is (= 25 r) "function return should be right.")
     (is (= [[:fn-call "cljs.storm.tests-code.types" "area" [s] 2018231146]
-            [:expr-exec 5 "4,2,1" 2018231146]
-            [:expr-exec 5 "4,2,2" 2018231146]
-            [:fn-return 25 "4,2" 2018231146]]
+            [:expr-exec 5 "4,2,1"]
+            [:expr-exec 5 "4,2,2"]
+            [:fn-return 25 "4,2"]]
            (u/capture)) "captured traces should match.")))
 
 (deftest deftype-test
   (let [r (ts/area (ts/->Circle 2))]
     (is (= 12.56 r) "function return should be right.")
     (is (= [[:fn-call "cljs.storm.tests-code.types" "area" ["#object[...]"] -1182932616]
-            [:expr-exec 2 "4,2,2" -1182932616]
-            [:expr-exec 2 "4,2,3" -1182932616]
-            [:fn-return 12.56 "4,2" -1182932616]]
+            [:expr-exec 2 "4,2,2"]
+            [:expr-exec 2 "4,2,3"]
+            [:fn-return 12.56 "4,2"]]
            (u/capture)) "captured traces should match.")))
 
 (deftest extend-type-basic-test
@@ -29,8 +29,8 @@
     (is (= 9 r) "function return should be right.")
     (is (= [[:fn-call "cljs.storm.tests-code.types" "area" ["tha-shape"] 1515835378]
             [:bind "s" "tha-shape" "3"]
-            [:expr-exec "tha-shape" "3,2,1" 1515835378]
-            [:fn-return 9 "3,2" 1515835378]]
+            [:expr-exec "tha-shape" "3,2,1"]
+            [:fn-return 9 "3,2"]]
            (u/capture)) "captured traces should match.")))
 
 (deftest extend-type-proto-test
@@ -38,8 +38,8 @@
         r (ts/sides-count tr)]
     (is (= 3 r) "function return should be right.")
     (is (= [[:fn-call "cljs.storm.tests-code.types" "sides-count" [tr] 1211097684]
-            [:expr-exec 2 "3,2,2" 1211097684]
-            [:fn-return 3 "3,2" 1211097684]]
+            [:expr-exec 2 "3,2,2"]
+            [:fn-return 3 "3,2"]]
            (u/capture)) "captured traces should match.")))
 
 (deftest extend-proto-basic-test
@@ -47,9 +47,9 @@
     (is (= 100 r) "function return should be right.")
     (is (= [[:fn-call "cljs.storm.tests-code.types" "area" [10] 1752788479]
             [:bind "n" 10 ""]
-            [:expr-exec 10 "8,2,1" 1752788479]
-            [:expr-exec 10 "8,2,2" 1752788479]
-            [:fn-return 100 "8,2" 1752788479]]
+            [:expr-exec 10 "8,2,1"]
+            [:expr-exec 10 "8,2,2"]
+            [:fn-return 100 "8,2"]]
            (u/capture)) "captured traces should match.")))
 
 (deftest extend-proto-type-test
@@ -59,9 +59,9 @@
     (is (= [[:fn-call "cljs.storm.tests-code.types" "area" [rect] 1752788479]
             [:bind "r" rect "3"]
             [:bind "r" rect ""] ;; TODO: hmm why is this?
-            [:expr-exec rect "3,2,1,1" 1752788479]
-            [:expr-exec 2 "3,2,1" 1752788479]
-            [:expr-exec rect "3,2,2,1" 1752788479]
-            [:expr-exec 4 "3,2,2" 1752788479]
-            [:fn-return 8 "3,2" 1752788479]]
+            [:expr-exec rect "3,2,1,1"]
+            [:expr-exec 2 "3,2,1"]
+            [:expr-exec rect "3,2,2,1"]
+            [:expr-exec 4 "3,2,2"]
+            [:fn-return 8 "3,2"]]
            (u/capture)) "captured traces should match.")))
