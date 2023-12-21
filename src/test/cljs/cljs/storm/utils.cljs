ade36c0aab6eef482c4f0b0b6fab268e103dde15
Juan Monetta <jpmonettas@gmail.com>
2023-12-21 15:27:11 -0300

    Fix tests after removing form-id from expr and return

--- src/test/cljs/cljs/storm/utils.cljs
+++ src/test/cljs/cljs/storm/utils.cljs
@@ -18,14 +18,14 @@
       (fn [_ form-ns form-name args form-id]
         (swap! captured-traces conj [:fn-call form-ns form-name (into [] (map maybe-serialize) args) form-id])))
 (set! cljs.storm.tracer/trace-expr-fn
-      (fn [_ val coord form-id]
-        (swap! captured-traces conj [:expr-exec (maybe-serialize val) coord form-id])))
+      (fn [_ val coord _]
+        (swap! captured-traces conj [:expr-exec (maybe-serialize val) coord])))
 (set! cljs.storm.tracer/trace-bind-fn
       (fn [_ coord sym-name val]
         (swap! captured-traces conj [:bind sym-name (maybe-serialize val) coord])))
 (set! cljs.storm.tracer/trace-fn-return-fn
-      (fn [_ ret-val coord form-id]
-        (swap! captured-traces conj [:fn-return (maybe-serialize ret-val) coord form-id])))
+      (fn [_ ret-val coord _]
+        (swap! captured-traces conj [:fn-return (maybe-serialize ret-val) coord])))
 
 (defn reset-captured-traces-fixture [f]
   (reset! captured-traces [])
